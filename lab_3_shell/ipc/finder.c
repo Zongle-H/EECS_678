45a44bef8e454bd9f7550cf2c24d29145561d114
enichols <e992n056@ku.edu>
2016-02-12 14:57:30 -0600

    it's working baby

--- lab_3_shell/ipc/finder.c
+++ lab_3_shell/ipc/finder.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
    int fd_2[2];
    int fd_3[2];
 
-   /* set up pipes */
+   /*
    if(pipe(fd_1) < 0) {
      printf("Couldn't make first pipe\n");
      return -1;
@@ -43,27 +43,24 @@ int main(int argc, char *argv[])
    };
 
    if(pipe(fd_3) < 0){
-     printf("Couldn't make second pipe\n");
+     printf("Couldn't make third pipe\n");
    };
+   set up pipes */
 
+  pipe(fd_1);
   pid_1 = fork();
   if (pid_1 == 0) {
 
     /* First Child */
     char cmdbuf[BSIZE];
     bzero(cmdbuf, BSIZE);
-    sprintf(cmdbuf, "%s %s -name \'*\'[ch]", FIND_EXEC, argv[1]);
+    sprintf(cmdbuf, "%s %s -name \'*\'.[ch]", FIND_EXEC, argv[1]);
 
-    //assign STD_OUT to be the write end of the first pipe
-    dup2(STDOUT_FILENO, fd_1[1]);
+    //assign write end of first pipe to be STD_OUT destination
+    dup2(fd_1[WRITE_END], STDOUT_FILENO);
 
-    //close all unused pipes
-    close(fd_1[WRITE_END]);
+    //close READ_END of pipe 1
     close(fd_1[READ_END]);
-    close(fd_2[WRITE_END]);
-    close(fd_2[READ_END]);
-    close(fd_3[WRITE_END]);
-    close(fd_3[READ_END]);
 
     if( (execl(BASH_EXEC, BASH_EXEC, "-c", cmdbuf, (char*) 0)) < 0) {
       fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
@@ -73,41 +70,73 @@ int main(int argc, char *argv[])
     exit(0);
   }
 
+  //close write end of first pipe
+  close(fd_1[WRITE_END]);
+
+  pipe(fd_2);
   pid_2 = fork();
   if (pid_2 == 0) {
     /* Second Child */
     char cmdbuf[BSIZE];
     bzero(cmdbuf, BSIZE);
-    sprintf(cmdbuf, "%s %s  xargs grep -c", XARGS_EXEC, argv[2]);
+    sprintf(cmdbuf, "%s %s -c %s", XARGS_EXEC, GREP_EXEC, argv[2]);
 
-    //close all unused pipes
-    close(fd_1[WRITE_END]);
-    close(fd_1[READ_END]);
-    close(fd_2[WRITE_END]);
-    close(fd_2[READ_END]);
-    close(fd_3[WRITE_END]);
-    close(fd_3[READ_END]);
+    //assign read end of first pipe to be desination of STD_IN
+    dup2(fd_1[READ_END], STDIN_FILENO);
+
+    //assign write end of second pipe to be destination of STD_OUT
+    dup2(fd_2[WRITE_END], STDOUT_FILENO);
+
+    if( (execl(BASH_EXEC, BASH_EXEC, "-c", cmdbuf, (char*) 0)) < 0) {
+      fprintf(stderr, "\nError execing xargs grep. ERROR#%d\n", errno);
+      return EXIT_FAILURE;
+    }
+    exit(0);
+  }
+
+  close(fd_1[READ_END]);
+  close(fd_2[WRITE_END]);
+
+  pipe(fd_3);
+  pid_3 = fork();
+  if (pid_3 == 0) {
+    /* Third Child */
+    char cmdbuf[BSIZE];
+    bzero(cmdbuf, BSIZE);
+    sprintf(cmdbuf, "%s -t: +1.0 -2.0 --numeric --reverse", SORT_EXEC);
+
+    dup2(fd_2[READ_END], STDIN_FILENO);
+    dup2(fd_3[WRITE_END], STDOUT_FILENO);
 
     if( (execl(BASH_EXEC, BASH_EXEC, "-c", cmdbuf, (char*) 0)) < 0) {
       fprintf(stderr, "\nError execing xargs grep. ERROR#%d\n", errno);
       return EXIT_FAILURE;
     }
 
-
     exit(0);
   }
 
-  pid_3 = fork();
-  if (pid_3 == 0) {
-    /* Third Child */
-    exit(0);
-  }
+  close(fd_2[READ_END]);
+  close(fd_3[WRITE_END]);
 
   pid_4 = fork();
   if (pid_4 == 0) {
     /* Fourth Child */
+    char cmdbuf[BSIZE];
+    bzero(cmdbuf, BSIZE);
+    sprintf(cmdbuf, "%s --lines=%s", HEAD_EXEC, argv[3]);
+
+    dup2(fd_3[READ_END], STDIN_FILENO);
+
+    if( (execl(BASH_EXEC, BASH_EXEC, "-c", cmdbuf, (char*) 0)) < 0) {
+      fprintf(stderr, "\nError execing xargs grep. ERROR#%d\n", errno);
+      return EXIT_FAILURE;
+    }
+
     exit(0);
   }
+
+  close(fd_3[READ_END]);
 
   if ((waitpid(pid_1, &status, 0)) == -1) {
     fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
